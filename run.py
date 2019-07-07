bdbd225ac8b1cf2e380190cb8e2b0c2885e90b17
yoshimitsu-san <jjvtapps@gmail.com>
2019-07-07 20:12:24 +0200

    initial version of working app

---
+++ run.py
@@ -0,0 +1,8 @@
+# Add your settings here... this is a temporary location, as the settings for a Flask app
+# should be stored separate from your main program.
+DEBUG = True
+
+from project import app
+
+if __name__ == "__main__":
+   app.run()
