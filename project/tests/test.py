85f42680dfa235d8a6a542743882b7eb55813006
yoshimitsu-san <jjvtapps@gmail.com>
2019-07-08 20:48:00 +0200

    Add initial unit test file

---
+++ project/tests/test.py
@@ -0,0 +1,30 @@
+
+import unittest
+
+from project import app
+
+class ProjectTests(unittest.TestCase):
+    #execute prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+        self.assertEquals(app.debug, False)
+
+    #execute after each test_client
+    def tearDown(self):
+        pass
+
+    # helper methods
+
+    #tests
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertIn(b'HOME page', response.data)
+        self.assertIn(b'topic 1', response.data)
+        self.assertIn(b'topic 2', response.data)
+        self.assertIn(b'topic 3', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
